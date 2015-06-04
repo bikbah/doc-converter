776090bbdd8523483d466394b0fcc044c54f8571
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-04 18:51:08 +0500

    main.go: Add getText() func.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"reflect"
+	"strconv"
 
 	"github.com/bikbah/go-php-serialize/phpserialize"
 )
@@ -19,7 +20,7 @@ var (
 
 func main() {
 	// Read php serialized file
-	dat, err := ioutil.ReadFile("1")
+	dat, err := ioutil.ReadFile("2")
 	if err != nil {
 		log.Fatalf("Read file error: %v", err)
 	}
@@ -35,11 +36,12 @@ func main() {
 		log.Fatal("interface{} to map[interface{}]interface{} assertion fail..")
 	}
 	convertedMap := parseRawMap(m)
-	// convertedMap["e"] = convertedMap["e"].(map[string]interface{})
-	// tmp := convertedMap["e"].(map[string]interface{})
-	fmt.Println(convertedMap["e"])
-	// fmt.Println(tmp["0"])
-	// fmt.Println(reflect.TypeOf(convertedMap))
+	wholeText := getText(convertedMap)
+	fmt.Println(wholeText)
+
+	if err := ioutil.WriteFile("out.html", []byte(wholeText), 0644); err != nil {
+		log.Fatal("Write file error")
+	}
 
 	// See key types
 	// keysCount = make(map[string]int)
@@ -77,6 +79,47 @@ func parseRawMap(m map[interface{}]interface{}) map[string]interface{} {
 	return res
 }
 
+func getText(m map[string]interface{}) (res string) {
+
+	res = ""
+
+	// Appending own texts first
+	textMapRaw, ok := m["text"]
+	if ok {
+		textMap, ok := textMapRaw.(map[string]interface{})
+		checkAssert(ok)
+		for i := 0; i < len(textMap); i++ {
+			textChildRaw, ok := textMap[strconv.Itoa(i)]
+			if !ok {
+				log.Fatal("getText error")
+			}
+			textChild, ok := textChildRaw.(map[string]interface{})
+			checkAssert(ok)
+			if textBody, ok := textChild["_"]; ok {
+				res += fmt.Sprintf("%v", textBody)
+			}
+		}
+	}
+
+	// Then append texts of children
+	childrenRaw, ok := m["e"]
+	if !ok {
+		return
+	}
+	children, ok := childrenRaw.(map[string]interface{})
+	checkAssert(ok)
+	for i := 0; i < len(children); i++ {
+		childRaw, ok := children[strconv.Itoa(i)]
+		if !ok {
+			log.Fatal("getText error")
+		}
+		child, ok := childRaw.(map[string]interface{})
+		checkAssert(ok)
+		res += getText(child)
+	}
+	return
+}
+
 func tmpFunc(m map[string]interface{}) {
 	for _, v := range m {
 		// log.Println(reflect.TypeOf(v).String())
@@ -95,5 +138,12 @@ func tmpFunc(m map[string]interface{}) {
 			}
 			tmpFunc(vv)
 		}
+	}
+}
+
+func checkAssert(ok bool) {
+	if !ok {
+		panic("assert")
+		// log.Fatal("Assertion error..")
 	}
 }
