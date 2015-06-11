98ed7e19ab562570a16d689c499ecc80606b466f
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-11 15:28:51 +0500

    main.go: Add Attribute.parse() method.

--- main.go
+++ main.go
@@ -125,9 +125,39 @@ func (e *E) parse(i interface{}) {
 func (e *E) addAttribute(attr Attribute) {
 	e.attribute = append(e.attribute, attr)
 }
+
 func (e *E) addChildE(child *E) {
 	e.e = append(e.e, child)
 }
+
+func (attr *Attribute) parse(i interface{}) {
+	m, ok := i.(map[interface{}]interface{})
+	if !ok {
+		panic("Assert error in Attribute.parse()")
+	}
+
+	dollar, ok := m["$"]
+	if !ok {
+		panic("Attribute has no $ field..")
+	}
+
+	dollarMap, ok := dollar.(map[interface{}]interface{})
+	if !ok {
+		panic("Assert error in Attribute.parse()")
+	}
+
+	attrName, ok := dollarMap["name"]
+	if !ok {
+		panic("Attribute has no NAME field..")
+	}
+
+	attrValue, ok := dollarMap["value"]
+	if !ok {
+		panic("Attribute has no VALUE field..")
+	}
+
+	attr.name = fmt.Sprint(attrName)
+	attr.value = fmt.Sprint(attrValue)
 }
 
 // Recursive function parseRawMap parses raw map.
