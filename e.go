3c7256d291308ed2d2262a41e9402e2df497aed4
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-11 18:00:12 +0500

    e.go: Add E.getText() method.

--- e.go
+++ e.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
+)
 
 type E struct {
 	dollap    map[string]string
 	attribute []Attribute
 	e         []*E
 	text      []Text
+	level     int
 }
 
 func (e *E) parse(i interface{}) {
@@ -96,4 +104,72 @@ func (e *E) addText(text Text) {
 
 func (e *E) addChildE(child *E) {
 	e.e = append(e.e, child)
+}
+
+func (e *E) getText() string {
+	attrMap := make(map[string]string)
+	for _, attr := range e.attribute {
+		attrMap[attr.name] = attr.value
+	}
+
+	textBody := ""
+	for _, text := range e.text {
+		textBody += text.getText()
+	}
+
+	rootNode, err := html.Parse(strings.NewReader(textBody))
+	if err != nil {
+		panic("Parse all text elements error")
+	}
+
+	var buf bytes.Buffer
+	htmlBody := rootNode.FirstChild.LastChild
+	paragraphID, ok := e.dollap["id"]
+	if !ok {
+		paragraphID = "e" + getRandomID(8)
+	}
+
+	for c := htmlBody.FirstChild; c != nil; c = c.NextSibling {
+		c.Attr = append(c.Attr, html.Attribute{Key: "id", Val: paragraphID})
+		c.Attr = append(c.Attr, html.Attribute{Key: "dd-level", Val: strconv.Itoa(e.level)})
+
+		styleDefault := make(map[string]string)
+		styleCustom := make(map[string]string)
+		for _, attr := range c.Attr {
+			if attr.Key == "style" {
+				styleDefault = parseCSS(attr.Val)
+			}
+		}
+		for k, v := range attrMap {
+			if k == "style" {
+				styleCustom = parseCSS(v)
+				for prop, val := range styleDefault {
+					styleCustom[prop] = val
+				}
+				c.Attr = append(c.Attr,
+					html.Attribute{
+						Key: "style",
+						Val: getCSS(styleCustom),
+					})
+				continue
+			}
+			c.Attr = append(c.Attr,
+				html.Attribute{
+					Key: k,
+					Val: v,
+				})
+		}
+
+		html.Render(&buf, c)
+		buf.WriteString("\n")
+		paragraphID = "e" + getRandomID(8)
+	}
+
+	ownText := buf.String()
+	childrenText := ""
+	for _, child := range e.e {
+		childrenText += child.getText()
+	}
+
+	return ownText + childrenText
 }
