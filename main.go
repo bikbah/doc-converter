f41a97c97270adbf86f1f4b36f1d761ad32b929f
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-11 15:58:59 +0500

    main.go: cut structs to separate files.

--- main.go
+++ main.go
@@ -27,18 +27,6 @@ var (
 	r         *rand.Rand
 )
 
-type E struct {
-	dollap    map[string]string
-	attribute []Attribute
-	e         []*E
-	text      []Text
-}
-
-type Attribute struct {
-	name  string
-	value string
-}
-
 type Text struct {
 	attribute    []Attribute
 	text         string
@@ -72,100 +60,6 @@ func main() {
 	convertedMap := parseRawMap(m)
 	wholeText := getText(convertedMap, 0)
 	saveData(wholeText)
-}
-
-func (e *E) parse(i interface{}) {
-	m, ok := i.(map[interface{}]interface{})
-	if !ok {
-		panic("Assert error in E.parse()")
-	}
-
-	for k, v := range m {
-		strKey := fmt.Sprintf("%v", k)
-
-		vv, ok := v.(map[interface{}]interface{})
-		if !ok {
-			panic("Assert error in E.parse()")
-		}
-
-		switch strKey {
-		case "$":
-			e.dollap = make(map[string]string)
-
-			for dollarK, dollarV := range vv {
-				dollarKStr := fmt.Sprint(dollarK)
-				dollarVStr := fmt.Sprint(dollarV)
-
-				e.dollap[dollarKStr] = dollarVStr
-			}
-			break
-		case "attribute":
-			for _, attrV := range vv {
-				attr := Attribute{}
-				attr.parse(attrV)
-				e.addAttribute(attr)
-			}
-			break
-		case "e":
-			eMap := make(map[string]interface{})
-
-			for eK, eV := range vv {
-				eKStr := fmt.Sprint(eK)
-				eMap[eKStr] = eV
-			}
-
-			for i := 0; i < len(eMap); i++ {
-				eKey := fmt.Sprint(i)
-				childInterface, ok := eMap[eKey]
-				if !ok {
-					panic("Invalid index in children e..")
-				}
-
-				childE := E{}
-				childE.parse(childInterface)
-				e.addChildE(&childE)
-			}
-			break
-		}
-	}
-}
-
-func (e *E) addAttribute(attr Attribute) {
-	e.attribute = append(e.attribute, attr)
-}
-
-func (e *E) addChildE(child *E) {
-	e.e = append(e.e, child)
-}
-
-func (attr *Attribute) parse(i interface{}) {
-	m, ok := i.(map[interface{}]interface{})
-	if !ok {
-		panic("Assert error in Attribute.parse()")
-	}
-
-	dollar, ok := m["$"]
-	if !ok {
-		panic("Attribute has no $ field..")
-	}
-
-	dollarMap, ok := dollar.(map[interface{}]interface{})
-	if !ok {
-		panic("Assert error in Attribute.parse()")
-	}
-
-	attrName, ok := dollarMap["name"]
-	if !ok {
-		panic("Attribute has no NAME field..")
-	}
-
-	attrValue, ok := dollarMap["value"]
-	if !ok {
-		panic("Attribute has no VALUE field..")
-	}
-
-	attr.name = fmt.Sprint(attrName)
-	attr.value = fmt.Sprint(attrValue)
 }
 
 // Recursive function parseRawMap parses raw map.
