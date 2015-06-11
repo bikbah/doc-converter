7fab4ab1a75ea1d7f13a37047d47c8d2607188bc
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-11 18:00:55 +0500

    text.go: Add Text.getText() method.

--- text.go
+++ text.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"golang.org/x/net/html"
+)
 
 type Text struct {
 	attribute    []Attribute
@@ -58,4 +64,82 @@ func (text *Text) parse(i interface{}) {
 
 func (text *Text) addAttribute(attr Attribute) {
 	text.attribute = append(text.attribute, attr)
+}
+
+func (text *Text) getText() string {
+	if !text.isInputField {
+		return text.text
+	}
+
+	rootNode, err := html.Parse(strings.NewReader("<span></span>"))
+	if err != nil {
+		panic("Parse span element error")
+	}
+
+	spanNode := rootNode.FirstChild.LastChild.FirstChild
+	attrMapStr := make(map[string]string)
+	for _, attr := range text.attribute {
+		attrNameStr := strings.ToLower(attr.name)
+		attrMapStr[attrNameStr] = attr.value
+	}
+
+	datafld, ok := attrMapStr["datafld"]
+	if !ok {
+		panic("Span has no datafld attribute..")
+	}
+	spanNode.Attr = append(spanNode.Attr,
+		html.Attribute{
+			Key: "dd-field",
+			Val: datafld,
+		})
+
+	id, ok := attrMapStr["id"]
+	if !ok {
+		id = "df" + getRandomID(16)
+	}
+	spanNode.Attr = append(spanNode.Attr,
+		html.Attribute{
+			Key: "id",
+			Val: id,
+		})
+
+	if source, ok := attrMapStr["source"]; ok {
+		spanNode.Attr = append(spanNode.Attr,
+			html.Attribute{
+				Key: "dd-source",
+				Val: source[1:],
+			})
+	}
+
+	if hint, ok := attrMapStr["hint"]; ok {
+		spanNode.Attr = append(spanNode.Attr,
+			html.Attribute{
+				Key: "dd-hint",
+				Val: hint,
+			})
+	}
+
+	if replText, ok := attrMapStr["replacementtext"]; ok {
+		spanNode.Attr = append(spanNode.Attr,
+			html.Attribute{
+				Key: "dd-replacementtext",
+				Val: replText,
+			})
+	}
+
+	for k, v := range attrMapStr {
+		if k == "rtl" || k == "id" || k == "datafld" || k == "source" || k == "hint" || k == "replacementtext" {
+			continue
+		}
+		spanNode.Attr = append(spanNode.Attr,
+			html.Attribute{
+				Key: k,
+				Val: v,
+			})
+	}
+
+	var buf bytes.Buffer
+	html.Render(&buf, spanNode)
+
+	return buf.String()
 }
