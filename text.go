de75c7cb01346e571076528b71b4bc5c90dd8a3e
Bahtiyar Biksayev <bahtiyar.biksayev@gmail.com>
2015-06-11 16:55:11 +0500

    text.go: Add Text.addAttribute() method.

--- text.go
+++ text.go
@@ -4,4 +4,6 @@ type Text struct {
 	attribute    []Attribute
 	text         string
 	isInputField bool
+func (text *Text) addAttribute(attr Attribute) {
+	text.attribute = append(text.attribute, attr)
 }
